46eae2fdc387c751726e0fc883909dc3c1f6048f
Adam Kearney <aklives@hotmail.com>
2019-03-16 07:02:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,13 @@ end
 def summon_captain_planet array
   array.map do |x|
     x.capitalize + "!"
-  end    
+  end
 end
 
 def long_planeteer_calls array
   array.any? do |x|
     x.length > 4
-  end    
+  end
 end
 
 def find_the_cheese# code an argument here
